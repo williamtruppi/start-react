4efb6fd01e4692ffafef0ef4714888c5f44f7b4d
William Truppi <72492620+williamtruppi@users.noreply.github.com>
2021-04-12 16:01:10 +0200

    use object inside an inline style

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import ReactDOM from "react-dom";
 const name = "William";
 const date = new Date();
 
+const listStyle = {
+  backgroundColor: "yellow",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
 ReactDOM.render(
   <div>
     <h1 className="titleColor" contentEditable="true" spellCheck="false">
       {name}'s favourite MCU Movies
     </h1>
-    <ul>
+    <ul style={listStyle}>
       <li>Avengers: Infinity War</li>
       <li>Spiderman: Far From Home</li>
       <li>Captain America: Civil War</li>
