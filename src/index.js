1012e96edd697feef28f1b33157e911f54b2f97a
William Truppi <72492620+williamtruppi@users.noreply.github.com>
2021-04-12 10:34:44 +0200

    pass variable with js syntax

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "William";
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite MCU Movies</h1>
+    <h1>{name}'s favourite MCU Movies</h1>
     <ul>
       <li>Avengers: Infinity War</li>
       <li>Spiderman: Far From Home</li>
