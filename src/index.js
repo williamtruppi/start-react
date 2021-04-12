2a4bb1daa46238663d6703223535e972e3407384
William Truppi <72492620+williamtruppi@users.noreply.github.com>
2021-04-12 10:52:28 +0200

    add date variable and insert into jsx with function getFullYear

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name = "William";
+const date = new Date();
 
 ReactDOM.render(
   <div>
@@ -11,6 +12,7 @@ ReactDOM.render(
       <li>Spiderman: Far From Home</li>
       <li>Captain America: Civil War</li>
     </ul>
+    <h6>Copyright {date.getFullYear()}</h6>
   </div>,
   document.getElementById("root")
 );
